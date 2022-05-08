2b5da30535a53aae187cdb6559c6fca74d7a924c
Igor_Julava <igor.rokobit@gmail.com>
2022-05-08 19:03:12 +0300

    d

---
+++ test/MyServer.h
@@ -0,0 +1,16 @@
+//
+// Created by Nekbakht Zabirov on 08.05.2022.
+//
+
+#ifndef UNTITLED1_MYSERVER_H
+#define UNTITLED1_MYSERVER_H
+
+
+class MyServer {
+public:
+    void setup();
+    void run();
+};
+
+
+#endif //UNTITLED1_MYSERVER_H
