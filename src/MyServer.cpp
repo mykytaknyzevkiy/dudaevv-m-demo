c831c99c30786a75ff01feca97183798e1b984b4
nekbakhtzabirov <nikita.knyazevkiy@wisercat.eu>
2022-05-08 19:08:18 +0300

    clear

--- src/MyServer.cpp
+++ src/MyServer.cpp
@@ -17,6 +17,8 @@
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
+String valueString = String(5);
+
 class CaptiveRequestHandler : public AsyncWebHandler {
 public:
     CaptiveRequestHandler() {}
@@ -29,12 +31,38 @@ public:
 
     void handleRequest(AsyncWebServerRequest *request) {
         AsyncResponseStream *response = request->beginResponseStream("text/html");
-        response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
-        response->print("<p>This is out captive portal front page.</p>");
-        response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
-        response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
-        response->print("</body></html>");
+
+        response->println("HTTP/1.1 200 OK");
+        response->println("Content-type:text/html");
+        response->println("Connection: close");
+        response->println();
+
+        // Display the HTML web page
+        response->println("<!DOCTYPE html><html>");
+        response->println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+        response->println("<link rel=\"icon\" href=\"data:,\">");
+        // CSS to style the on/off buttons
+        // Feel free to change the background-color and font-size attributes to fit your preferences
+        response->println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+        response->println(".slider { width: 300px; }</style>");
+        response->println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+
+        // Web Page
+        response->println("</head><body><h1>ESP32 with Servo</h1>");
+        response->println("<p>Position: <span id=\"servoPos\"></span></p>");
+        response->println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
+
+        response->println("<script>var slider = document.getElementById(\"servoSlider\");");
+        response->println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
+        response->println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
+        response->println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+        response->println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+
+        response->println("</body></html>");
+
         request->send(response);
+
+        Serial.println(request->url())
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,16 @@
 #include <Wings.h>
 #include <Arduino.h>
 #include <MyServer.h>
-#include <Webservo.h>
 
 DriverMotor motorSpeed = DriverMotor();
 Wings wings = Wings();
 
 MyServer myServer = MyServer();
-Webservo webservo = Webservo();
 
 void setup() {
     wings.setup();
     motorSpeed.setup();
-    //myServer.setup();
+    myServer.setup();
 
 }
 
