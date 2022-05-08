b8811d58efbb5723368eaf81afb79dc9e94cd658
Igor_Julava <igor.rokobit@gmail.com>
2022-05-08 18:27:26 +0300

    diff

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,21 @@
 #include <Wings.h>
 #include <Arduino.h>
 #include <MyServer.h>
+#include <Webservo.h>
 
 DriverMotor motorSpeed = DriverMotor();
 Wings wings = Wings();
 
 MyServer myServer = MyServer();
+Webservo webservo = Webservo();
 
 void setup() {
     wings.setup();
     motorSpeed.setup();
-    myServer.setup();
+    //myServer.setup();
+
 }
 
 void loop() {
     myServer.run();
 }
-
